8051392082f18458b237a5f4e65a97a3cd5017fe
donddy243 <makasij21@gmail.com>
2022-06-30 12:48:34 +0200

    mon premier commit

---
+++ myshop_fl/src/Entity/Produit.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $description;
+
+    #[ORM\Column(type: 'integer')]
+    private $prix;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $city;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $code_postal;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $reservation_text;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdProduit(): ?int
+    {
+        return $this->id_produit;
+    }
+
+    public function setIdProduit(int $id_produit): self
+    {
+        $this->id_produit = $id_produit;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(string $code_postal): self
+    {
+        $this->code_postal = $code_postal;
+
+        return $this;
+    }
+
+    public function getReservationText(): ?string
+    {
+        return $this->reservation_text;
+    }
+
+    public function setReservationText(string $reservation_text): self
+    {
+        $this->reservation_text = $reservation_text;
+
+        return $this;
+    }
+}
